2d472ed49615d230d600aa64af2f37870c045560
wangkai <646651700@qq.com>
2019-05-15 23:48:36 +0800

    base64加密

---
+++ src/test/java/com/wufeng/base64/TestBase64.java
@@ -0,0 +1,17 @@
+package com.wufeng.base64;
+
+import java.util.Base64;
+
+/**
+ * @Author wangkai
+ * @CreateTime 2019-05-15 23:47
+ **/
+public class TestBase64 {
+    public static void main(String[] args) {
+//        Base64.Decoder decoder = Base64.getDecoder();
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] contentOfByte = "8888".getBytes();
+        String temp = encoder.encodeToString(contentOfByte);
+        System.out.println(temp);
+    }
+}
